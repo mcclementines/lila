d493630c28b6d8fc3681f7e1959b1b54b26b5396
mcclementines <jonathanmcclement@gmail.com>
2023-08-28 22:34:43 -0400

    vite+react+typescript

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+export default {
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
