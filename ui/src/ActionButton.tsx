a35662019a15c13316f8038ab681e7b7f3db4700
mcclementines <jonathanmcclement@gmail.com>
2023-08-30 21:10:57 -0400

    fix

--- ui/src/ActionButton.tsx
+++ ui/src/ActionButton.tsx
@@ -1,4 +1,9 @@
-function ActionButton(props) {
+interface Props {
+  link: string;
+  text: string;
+}
+
+function ActionButton(props:Props) {
   return (
     <a className="text-center text-xl font-work-sans" href={props.link}>{props.text}</a>
   )
