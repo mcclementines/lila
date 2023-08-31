1496445f923da96c533d4ffbf133793914e8c342
mcclementines <jonathanmcclement@gmail.com>
2023-08-30 22:18:08 -0400

    vh fix

--- ui/postcss.config.cjs
+++ ui/postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-viewport-height-correction'),
+    require('autoprefixer')
+  ],
 }
