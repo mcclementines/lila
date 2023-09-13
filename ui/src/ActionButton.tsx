8c9360205d3890b7c06190ef4375532e58248137
mcclementines <jonathanmcclement@gmail.com>
2023-09-12 20:34:45 -0400

    homescreen complete

--- ui/src/ActionButton.tsx
+++ ui/src/ActionButton.tsx
@@ -1,11 +1,28 @@
 interface Props {
   link: string;
   text: string;
+  color: string;
 }
 
 function ActionButton(props:Props) {
+  let className = "shadow-md h-14 flex text-center text-xl font-work-sans rounded-xl border-b-4";
+
+  switch(props.color) {
+    case "primary":
+      className += " bg-indigo-600 border-indigo-700 text-white";
+      break;
+    case "secondary":
+      className += " border-t-2 border-x-2 bg-white border-gray-300 text-blue-400";
+      break;
+    default:
+      className += " bg-indigo-600 border-indigo-700 text-white";
+      break;
+  }
+   
   return (
-    <a className="text-center text-xl font-work-sans" href={props.link}>{props.text}</a>
+    <a className={className} href={props.link}>
+      <span className="align-middle m-auto">{props.text}</span>
+    </a>
   )
 }
 
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -8,22 +8,24 @@ function App() {
       <div className="w-full">
         <Nav />
       </div>
-      <div className="w-full h-[calc(calc(var(--vh,1vh)_*_100)_-_4rem)] justify-center px-8">
+      <div className="w-full max-w-lg h-[calc(calc(var(--vh,1vh)_*_100)_-_4rem)] justify-center px-8 mx-auto">
         <div className="flex h-[calc(var(--vh,1vh)_*_50)]">
-          <h1 className="text-center m-auto text-4xl font-work-sans">
-            Test prep books are so ______ boring, I just want to practice!
-          </h1>
+          <div className='flex mx-auto mt-auto bg-indigo-600 rounded-3xl drop-shadow-xl border-b-8 border-indigo-700 p-12'>
+            <h1 className="text-center m-auto text-4xl text-white font-work-sans">
+              Test prep books are so <span className='underline whitespace-pre'>          </span> boring, I just want to practice!
+            </h1>
+          </div>
         </div>
         <div className="flex flex-col h-[calc(calc(var(--vh,1vh)_*_50)_-_4rem)]">
           <div className="flex-1">
-            <h1 className="justify-center text-center text-3xl font-work-sans">
-              No Gimmicks. No Tricks. Just unlimited practice.
+            <h1 className="justify-center text-center text-3xl mt-8 text-gray-800 font-work-sans">
+              No Tricks. No Gimmicks. Just unlimited practice.
             </h1>
           </div>
           <div className="flex-none w-full mb-4">
             <div className="flex flex-col justify-center space-y-2">
-              <ActionButton link="/createAccount" text="LET'S GO!"/>
-              <ActionButton link="/login" text="I ALREADY HAVE AN ACCOUNT"/>
+              <ActionButton color="primary" link="/createAccount" text="LET'S GET STARTED"/>
+              <ActionButton color="secondary" link="/login" text="I ALREADY HAVE AN ACCOUNT"/>
             </div>
           </div>
         </div>
