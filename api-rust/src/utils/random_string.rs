a8248e63a0d4415e9c94997067637a8a553e99a2
mcclementines <jonathanmcclement@gmail.com>
2024-06-03 02:46:47 -0400

    updated random string, caniuse in npm

--- api-rust/src/utils/random_string.rs
+++ api-rust/src/utils/random_string.rs
@@ -1,19 +1,7 @@
 //! src/utils/random_string.rs
 
-use rand::{distributions::Uniform, thread_rng, Rng};
-
-const BASE62_CHARSET: &[u8] = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRZTUVWXYZ";
+use advanced_random_string::{ charset, random_string };
 
 pub fn generate_base62_string(length: usize) -> String {
-    let mut rng = thread_rng();
-    let uniform = Uniform::from(0..62);
-
-    let base62_string: String = (0..length)
-        .map(|_| {
-            let idx = rng.sample(uniform);
-            BASE62_CHARSET[idx] as char
-        })
-        .collect();
-
-    base62_string
+    random_string::generate_unsecure(length, charset::BASE62)
 }
