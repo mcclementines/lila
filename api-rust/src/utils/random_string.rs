1012a1679b34e724a6800c36d1f1556ba543db38
mcclementines <jonathanmcclement@gmail.com>
2024-06-03 02:50:23 -0400

    cargo fmt fix

--- api-rust/src/utils/random_string.rs
+++ api-rust/src/utils/random_string.rs
@@ -1,6 +1,6 @@
 //! src/utils/random_string.rs
 
-use advanced_random_string::{ charset, random_string };
+use advanced_random_string::{charset, random_string};
 
 pub fn generate_base62_string(length: usize) -> String {
     random_string::generate_unsecure(length, charset::BASE62)
